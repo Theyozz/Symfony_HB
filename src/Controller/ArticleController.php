b14a614b137b2220153a9f083e78f03f9684a4aa
Théo VM <theomaurin875@gmail.com>
2023-04-24 09:36:34 +0200

    article list added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,12 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    #[Route('/articles', name: 'articles_list')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+        return $this->render('article/items.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+
     #[Route('/article/{id}', name: 'article_content')]
     public function item(Article $article): Response
     {
